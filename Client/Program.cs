5c846c321047c36633fc78efd2faa140a62eb24f
gul.islamoglu <gul.islamoglu@IPAS-34-NGI-NB>
2024-10-07 11:25:55 +0300

    Filtering

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using SiparisApp.Client;
 
 namespace SiparisApp.Client
@@ -16,6 +17,7 @@ namespace SiparisApp.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddBlazoredModal();
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
