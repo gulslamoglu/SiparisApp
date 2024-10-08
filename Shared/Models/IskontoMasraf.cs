f83b407b850820b4fd465d3261f8054d504e058b
gul.islamoglu <gul.islamoglu@IPAS-34-NGI-NB>
2024-10-08 14:49:17 +0300

    İskonto/masraf

---
+++ Shared/Models/IskontoMasraf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiparisApp.Shared.Models
+{
+    public class IskontoMasraf
+    {
+        public string Turu { get; set; }
+        public string Hesaplama { get; set; }
+        public decimal Miktar { get; set; }
+        public decimal Hesaplanan { get; set; }
+    }
+}
