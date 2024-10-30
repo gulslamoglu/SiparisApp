9ca9b73d3108b129dc691b45cc0a6d8a370dea1e
gul.islamoglu <gul.islamoglu@IPAS-34-NGI-NB>
2024-10-30 17:08:30 +0300

    editing cariler

--- Shared/Models/Cariler/Cari.cs
+++ Shared/Models/Cariler/Cari.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SiparisApp.Shared.Models.Adress;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +24,8 @@ namespace SiparisApp.Shared.Models.Cariler
         public string Tip { get; set; } // Cari tipi (şahıs/kurum)
         public string TCKimlikNo { get; set; } // Şahıssa T.C. kimlik no
         public string VergiNo { get; set; } // Kurumsa vergi no
+        public List<Phone> Telefonlar { get; set; } = new List<Phone>();
+        public List<Email> Epostalar { get; set; } = new List<Email>();
+        public List<Address> Adresler { get; set; } = new List<Address>();
     }
 }
---
+++ Shared/Models/Cariler/CariModel.cs
@@ -0,0 +1,22 @@
+﻿using SiparisApp.Shared.Models.Adress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiparisApp.Shared.Models.Cariler
+{
+    public class CariModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TC { get; set; }
+        public string Unvan { get; set; }
+        public string Eposta { get; set; }
+        public string Telefon { get; set; }
+        public int AdresId { get; set; }
+        
+    }
+}
